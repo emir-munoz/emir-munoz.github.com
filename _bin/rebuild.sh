82a6a30a7fbd264159cacf1ea907997e8489a769
emir-munoz <emir@emunoz.org>
2012-11-29 23:22:31 +0000

    Release 1.2 home page

---
+++ _bin/rebuild.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /home/emir/Word/research/homepage/emir-munoz.github.com/
+
+# generate publications/index.html file
+php publications/body.php
+
+# run jekyll
+jekyll --no-auto
+
+# upload to branch
+git add .
+git commit -m "Release 1.2 home page"
+git push origin master
