b077618316588e74234afc6952a35fee460544b1
emir-munoz <emir@emunoz.org>
2017-08-17 20:31:06 +0100

    Change the publications style

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,10 @@
 (function($){
 	$(document).ready(function(){
-		
-	
+		$('.bibtex.thesis').parent('li').addClass('thesis');
+		$('.bibtex.journal').parent('li').addClass('journal');
+		$('.bibtex.conference').parent('li').addClass('conference');
+		$('.bibtex.workshop').parent('li').addClass('workshop');
+		$('.bibtex.poster_demo').parent('li').addClass('poster_demo');
+		$('.bibtex.patent').parent('li').addClass('patent');
 	});
 })(jQuery);
